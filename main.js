8870bef23bae03eaf16d14e2d3f7100b8eea8ef6
alisain110 <164510224+alisain110@users.noreply.github.com>
2024-03-26 04:31:13 +0500

    first number guess game

---
+++ main.js
@@ -0,0 +1,16 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "usernumberguessing",
+        type: "number",
+        message: " Guess a random number"
+    }
+]);
+if (answers.usernumberguessing === randomNumber) {
+    console.log("Congrats you did it");
+}
+else {
+    console.log("you lose it hahahaha");
+}
